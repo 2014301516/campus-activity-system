522309cfd49a4817bf270ed85d23c9d082f9a796
hoShi <2014301516@qq.com>
2026-06-24 18:10:55 +0800

    fix: 补加ActivityServiceImpl缺少的List导入

--- backend/src/main/java/com/cas/service/impl/ActivityServiceImpl.java
+++ backend/src/main/java/com/cas/service/impl/ActivityServiceImpl.java
@@ -21,6 +21,7 @@ import org.springframework.util.StringUtils;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
